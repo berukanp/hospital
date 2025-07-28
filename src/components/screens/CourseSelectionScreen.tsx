f699c3ea2eed2152952c7fdeae2f2953310214da
berukanp <42884998+berukanp@users.noreply.github.com>
2025-07-28 18:13:21 +0900

    Fix duplicate courseOptions declaration

--- src/components/screens/CourseSelectionScreen.tsx
+++ src/components/screens/CourseSelectionScreen.tsx
@@ -18,18 +18,6 @@ const CourseSelectionScreen: React.FC = () => {
     return null;
   }
   
-  const courseOptions: CourseOption[] = [
-    "生活習慣病予防健診",
-    "定期健診",
-    "法定定期健診",
-    "雇用時健診",
-    "簡易健診",
-    "特定健診",
-    "電離放射線健診",
-    "長時間勤務健診",
-    "その他"
-  ];
-  
   const courseOptions = [
     {
       name: "生活習慣病予防健診" as CourseOption,
